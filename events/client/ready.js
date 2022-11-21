434f8317bd5a197d2e1af0e5aad0b35ef2ffd33e
Woon Seng Hong <88629349+pendragons-code@users.noreply.github.com>
2022-11-21 18:58:28 +0800

    Add files via upload

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,4 +1,9 @@
+const { ActivityType } = require("discord.js")
 module.exports = async (Jasbot) =>{
-    console.log("Yes")
-    Jasbot.webserver
+	console.log("Yes")
+	Jasbot.webserver
+	Jasbot.user.setPresence({
+		activities: [{ name: `I’m Asher Quinn…`, type: ActivityType.Playing }],
+		status: 'idle',
+	});
 }
